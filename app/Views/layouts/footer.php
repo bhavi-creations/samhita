a928a8e5103f8ae69e35d5c1b9db4683d29cd123
Ramesh92340 <rameshpilli1428@gmail.com>
2025-06-24 16:13:51 +0530

    footer  updated

--- app/Views/layouts/footer.php
+++ app/Views/layouts/footer.php
@@ -2,7 +2,7 @@
 
      <strong>
          Copyright &copy;
-         <a href="https://bhavicreationspvtltd.com/" target="blank" class="text-decoration-none">Bhavi Creations
+         <a href="https://bhavicreationspvtltd.com/" target="blank" class="text-decoration-none">Bhavi Creations Pvt Ltd
              <img src="<?= base_url('assets/img/bhavi_logo/Bhavi_Branding_Stamp.png ') ?>" alt="Samhita Logo" style="width: 35px;" class="      " /></a>.
      </strong>
      All rights reserved.
